d3991ac0fd55462099da05110508a043b867462f
Senior Consultant <adefolarin.adeniji@andela.com>
2018-11-04 16:16:19 +0100

    Able to authenticate a user and generate token

--- user/UserController.js
+++ user/UserController.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('../config');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
@@ -20,12 +22,84 @@ router.post('/', function (req, res) {
         });
 });
 
+router.get('/me', function(req, res) {
+  var token = req.headers['x-access-token'];
+  console.log(token);
+
+  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+
+  jwt.verify(token, config.secret, function(err, decoded) {
+    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+
+    res.status(200).send(decoded);
+  });
+});
+
 // RETURNS ALL THE USERS IN THE DATABASE
 router.get('/', function (req, res) {
-    User.find({}, function (err, users) {
-        if (err) return res.status(500).send("There was a problem finding the users.");
-        res.status(200).send(users);
-    });
+
+  res.json({message: 'Welcome to the users api where you can look up a user'});
+
+});
+
+router.get('/users', function (req, res) {
+
+  var token = req.headers['x-access-token'];
+  console.log(token);
+
+  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+
+  jwt.verify(token, config.secret, function(err, decoded) {
+    if (err) {
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    } else {
+      User.find({}, function (err, users) {
+            if (err) return res.status(500).send("There was a problem finding the users.");
+            res.status(200).send(users);
+        });
+    }
+
+    //res.status(200).send(decoded);
+  });
+});
+
+// route to authenticate a user (POST http://localhost:3000/api/authenticate)
+router.post('/authenticate', function (req, res) {
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token with only our given payload
+    // we don't want to pass in the entire user since that has the password
+    const payload = {
+      admin: user.admin     };
+        var token = jwt.sign(payload, config.secret, {
+          expiresIn: 86400 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+    }
+
+  });
 });
 
 // GETS A SINGLE USER FROM THE DATABASE
